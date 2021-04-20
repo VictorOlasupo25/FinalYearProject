31d030f7cc09923d2440b71918405adc17ed423a
victor <victorolasupo25@gmail.com>
2021-04-20 10:49:18 +0100

    Code review and testing- App not fully running

---
+++ Final year/Software_Patterns/Assignment4/app/src/main/java/com/victorolasupo/assignment4/AddressAdapter.java
@@ -0,0 +1,4 @@
+package com.victorolasupo.assignment4;
+
+public class AddressAdapter {
+}
--- Final year/Software_Patterns/Assignment4/app/src/main/java/com/victorolasupo/assignment4/MainActivity.java
+++ Final year/Software_Patterns/Assignment4/app/src/main/java/com/victorolasupo/assignment4/MainActivity.java
@@ -14,106 +14,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText emaillogin, passwordlogin;
-    Button LOGIN;
-    ProgressDialog progressDialog;
-    String email;
-    String password;
-    TextView linksignup;
-    database db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        emaillogin = findViewById(R.id.input_email);
-        passwordlogin = findViewById(R.id.input_password);
-        LOGIN = findViewById(R.id.btn_login);
-        db = new database(MainActivity.this);
-        linksignup = findViewById(R.id.link_signup);
-        linksignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Registration.class));
-            }
-        });
-        LOGIN.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                login();
-            }
-        });
     }
 
-    public void login() {
 
-        if (!validate()) {
-
-            return;
-        }
-
-        LOGIN.setEnabled(false);
-
-        progressDialog = new ProgressDialog(MainActivity.this,
-                R.style.AppTheme_Dark_Dialog);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Checking Record...");
-        progressDialog.show();
-
-        email = emaillogin.getText().toString();
-        password = passwordlogin.getText().toString();
-//        Toast.makeText(this, email+password+"", Toast.LENGTH_SHORT).show();
-        // TODO: Implement your own authentication logic here.
-
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onLoginSuccess or onLoginFailed
-
-                        LOGIN.setEnabled(true);
-                        if (db.Authenticate(email, password) == true) {
-                            Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_LONG).show();
-                            Intent intent = new Intent(MainActivity.this, InsertData.class);
-                            intent.putExtra("getname",email);
-                            startActivity(intent);
-                            progressDialog.dismiss();
-                        } else {
-                            progressDialog.dismiss();
-                            Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
-                        }
-
-                        // onLoginFailed();
-
-                    }
-                }, 2000);
-    }
-
-    public boolean validate() {
-        boolean valid = true;
-
-        String email = emaillogin.getText().toString();
-        String password = passwordlogin.getText().toString();
-
-
-        if (email.isEmpty()) {
-            emaillogin.setError("Fill UserName");
-            valid = false;
-            Animation animShake = AnimationUtils.loadAnimation(MainActivity.this, R.anim.one);
-            LOGIN.startAnimation(animShake);
-        } else {
-            emaillogin.setError(null);
-        }
-
-        if (password.isEmpty()) {
-            passwordlogin.setError("Enter Password");
-            valid = false;
-            Animation animShake = AnimationUtils.loadAnimation(MainActivity.this, R.anim.one);
-            LOGIN.startAnimation(animShake);
-        } else {
-            passwordlogin.setError(null);
-        }
-
-        return valid;
-    }
 }
